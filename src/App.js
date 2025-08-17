77c12b67cbddf68e31fdd3b032f11b341255f980
Manojc234 <mc7801138@gmail.com>
2025-08-17 20:59:49 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+
+import Dashboard from "./Components/Dashboard";
+import "./Styles/custom.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Role will later come from backend
+  const role = "operator"; // admin | creator | approver
+  return <Dashboard role={role} />;
 }
 
 export default App;
